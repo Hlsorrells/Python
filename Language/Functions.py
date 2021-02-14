3a57a74cae8a47bece0346296ce2196e3c6c3258
Hlsorrells <hlsorrells.dev@gmail.com>
2021-02-14 16:18:00 -0500

    Example of defining a function

---
+++ Language/Functions.py
@@ -0,0 +1,22 @@
+# Functions
+
+# Define a function
+def greet(lang):
+    if lang == 'es':
+        return 'Hola'
+    elif lang == 'fr':
+        return 'Bonjour'
+    else:
+        return 'Hello'
+
+print(greet('en'), "Manual")
+
+def addtwo (a,b):
+    added = a + b
+    return added
+
+
+# Methods
+max()
+float()
+type()

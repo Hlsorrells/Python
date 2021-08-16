d437c0c588453383c2c62f86937cb28c1e7eaeb5
Hlsorrells <hlsorrells.dev@gmail.com>
2021-08-16 15:02:27 -0400

    added "is" vs "==" operators example

---
+++ Language/Operators/IS_Operator.py
@@ -0,0 +1,24 @@
+# Is and Is Not as operators
+
+# "is" vs "=="
+
+>>> a = [1, 2, 3]
+>>> b = a
+
+>>> a is b
+True
+>>> a == b
+True
+
+>>> c = list(a)
+
+>>> a == c
+True
+>>> a is c
+False
+
+# • "is" expressions evaluate to True if two 
+#   variables point to the same object
+
+# • "==" evaluates to True if the objects 
+#   referred to by the variables are equal
